abca71d85c03cf46e9486e1e4cbc53e577b470a5
statichobanov <sachnqqq@gmail.com>
2019-07-13 15:36:38 +0300

    Sticky header added

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,3 +2,14 @@ $(window).on("load", () => {
     $('.slot div').addClass('join-us');
 })
 
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= $('.header-wrapper').height()/2) {
+     
+        $('.header-wrapper').addClass('fixed-header');
+        $('.resizer').css('display','block');
+    }
+    else {
+        $('.header-wrapper').removeClass('fixed-header');
+        $('.resizer').css('display','none');
+    }
+});
