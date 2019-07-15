44122b873b1a7080e201ecf82dcd50a6fcdf7762
statichobanov <sachnqqq@gmail.com>
2019-07-15 19:18:52 +0300

    Testing datebase

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,13 @@
 const database = require('../config/database');
 
 module.exports = (req, res) => {
-    //const db = database.getDb();
+    const db = database.getDb();
 
-    //db.db().collection('emails').find({}).toArray((err, subscribers) => {
-    //    if(err) { console.log(err) };
-//
-    //});
-    res.render('home',{});
+    db.db().collection('emails').find({}).toArray((err, subscribers) => {
+        if (err) { console.log(err) };
+
+        res.render('home',{
+            subscribers
+        });
+    });
 }
