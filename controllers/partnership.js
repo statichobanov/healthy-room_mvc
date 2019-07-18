284f940fa48b762d4a064d49b3a40b022477d15a
statichobanov <sachnqqq@gmail.com>
2019-07-18 21:28:46 +0300

    master : removing useless code

--- controllers/partnership.js
+++ controllers/partnership.js
@@ -2,11 +2,6 @@ function partnership (req, res) {
     res.render('partnership', {layout: false});
 }
 
-function postNewMember (req, res) {
-
-}
-
 module.exports = {
     partnership: partnership,
-    postNewMember: postNewMember
 }
